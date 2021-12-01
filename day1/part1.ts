c7da476b8750ca37538b155247965c551ac14367
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-12-01 06:14:14 +0100

    Code for day 1

--- day1/part1.ts
+++ day1/part1.ts
@@ -5,9 +5,15 @@ export default class Part1 extends AoCPart {
     getPath = () => __dirname;
 
     async getAnswer(): Promise<string> {
-        const lines = await this.getLines();
+        const nums = await this.getLinesAsInt();
 
-        return "";
+        let count = 0;
+        let prevNum = 0;
+        let currentNum = 0;
+        for (let i = 0; i < nums.length; i++, currentNum = nums[i], prevNum = nums[i - 1])
+            if (currentNum > prevNum) count++;
+
+        return count.toString();
     }
 
 }
--- day1/part2.ts
+++ day1/part2.ts
@@ -5,9 +5,23 @@ export default class Part2 extends AoCPart {
     getPath = () => __dirname;
 
     async getAnswer(): Promise<string> {
-        const lines = await this.getLines();
+        const nums = await this.getLinesAsInt();
 
-        return "";
+        let count = 0;
+
+        let num1;
+        let num2;
+        let num3;
+        let num4;
+
+        for (let i = 0; i < nums.length - 3; i++, num1 = nums[i], num2 = nums[i + 1], num3 = nums[i + 2], num4 = nums[i + 3]) {
+            const windowA = num1 + num2 + num3;
+            const windowB = num2 + num3 + num4;
+            if (windowB > windowA)
+                count++;
+        }
+
+        return count.toString();
     }
 
 }
