248e32c287e6861f42f42637a77f91adab272d99
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-12-03 06:54:43 +0100

    Completed day 3

--- day3/part1.ts
+++ day3/part1.ts
@@ -1,3 +1,4 @@
+import { once } from "events";
 import { AoCPart } from "../common/AoCPart";
 
 export default class Part1 extends AoCPart {
@@ -7,7 +8,29 @@ export default class Part1 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        const nums = lines.map(line => line.split(''));
+
+        let gamma = "";
+        let epsilon = "";
+
+        for (let i = 0; i < nums[0].length; i++) {
+            let zeroCount = 0;
+            let oneCount = 0;
+            for (let j = 0; j < nums.length; j++) {
+                if (nums[j][i] == "0")
+                    zeroCount++;
+                else if (nums[j][i] == "1")
+                    oneCount++;
+            }
+
+            const mcm = oneCount >= zeroCount ? "1" : "0";
+            const lcm = oneCount >= zeroCount ? "0" : "1";
+
+            gamma += mcm;
+            epsilon += lcm;
+        }
+
+        return (parseInt(gamma, 2) * parseInt(epsilon, 2)).toString();
     }
 
 }
--- day3/part2.ts
+++ day3/part2.ts
@@ -1,3 +1,4 @@
+import { once } from "events";
 import { AoCPart } from "../common/AoCPart";
 
 export default class Part2 extends AoCPart {
@@ -7,7 +8,50 @@ export default class Part2 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        let oxygen = lines.slice();
+
+        let co2 = lines.slice();
+
+        for (let i = 0; i < lines[0].length; i++) {
+            /*
+                Go through index i of every number
+            */
+            let zeroCount = 0;
+            let oneCount = 0;
+            for (let j = 0; j < oxygen.length; j++) {
+                if (oxygen[j][i] == "0")
+                    zeroCount++;
+                else if (oxygen[j][i] == "1")
+                    oneCount++;
+            }
+
+            const mcm = oneCount >= zeroCount ? "1" : "0";
+
+            oxygen = oxygen.filter(str => str[i] == mcm);
+
+            if (oxygen.length == 1) break;
+        }
+
+        for (let i = 0; i < lines[0].length; i++) {
+            /*
+                Go through index i of every number
+            */
+            let zeroCount = 0;
+            let oneCount = 0;
+            for (let j = 0; j < co2.length; j++) {
+                if (co2[j][i] == "0")
+                    zeroCount++;
+                else if (co2[j][i] == "1")
+                    oneCount++;
+            }
+            const lcm = oneCount >= zeroCount ? "0" : "1";
+
+            co2 = co2.filter(str => str[i] == lcm);
+
+            if (co2.length == 1) break;
+        }
+
+        return (parseInt(oxygen[0], 2) * parseInt(co2[0], 2)).toString();
     }
 
 }
