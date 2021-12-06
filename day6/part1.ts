3659bd38c494576aea0da9ea0d3bcfffebb0d9a5
MrDragonPlays <thomasdvandeweerd@gmail.com>
2021-12-06 14:24:12 +0100

    Day 6

--- day6/part1.ts
+++ day6/part1.ts
@@ -7,7 +7,20 @@ export default class Part1 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        const fish: number[] = lines[0].split(",").map(str => +str);
+
+        for(let day = 1; day <= 80; day++){
+            const fishLength = fish.length;
+            for(let i = 0;i<fishLength;i++){
+                fish[i]--;
+                if(fish[i] == -1){
+                    fish[i] = 6;
+                    fish.push(8);
+                }
+            }
+        }
+
+        return fish.length.toString();
     }
 
 }
--- day6/part2.ts
+++ day6/part2.ts
@@ -7,7 +7,27 @@ export default class Part2 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        const fishes: number[] = lines[0].split(",").map(str => +str);
+
+        let fishMap: Map<number, number> = new Map(); // Days left : Amount
+
+        for (const fish of fishes) {
+            fishMap.set(fish, (fishMap.get(fish) || 0) + 1);
+        }
+
+        for (let day = 1; day <= 256; day++) {
+            const newFishMap: Map<number, number> = new Map();
+            for (const [fish, count] of Array.from(fishMap.entries())) {
+                if (fish == 0) {
+                    newFishMap.set(6, (newFishMap.get(6) || 0) + count);
+                    newFishMap.set(8, (newFishMap.get(8) || 0) + count);
+                } else {
+                    newFishMap.set(fish - 1, (newFishMap.get(fish - 1) || 0) + count);
+                }
+            }
+            fishMap = newFishMap;
+        }
+        return Array.from(fishMap.values()).reduce((a, b) => a + b).toString();
     }
 
 }
