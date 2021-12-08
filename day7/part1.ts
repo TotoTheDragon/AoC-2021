969b091f6f12dd46e69990d7f02fec71b1eb6066
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-12-08 08:35:52 +0100

    Day 7

--- day7/part1.ts
+++ day7/part1.ts
@@ -7,7 +7,26 @@ export default class Part1 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        const nums: number[] = lines[0].split(",").map(num => +num);
+
+        const obj = {};
+        const max = nums.reduce((a, b) => Math.max(a, b));
+        for (let i = 0; i <= max; i++) {
+            let fuel = 0;
+            for (let j = 0; j < nums.length; j++) {
+                fuel += Math.abs(i - nums[j]);
+            }
+            obj[i] = fuel;
+        }
+
+        const entries = Object.entries(obj);
+        let minEntry = entries[0];
+        for (const entry of entries) {
+            if (entry[1] < minEntry[1])
+                minEntry = entry;
+        }
+
+        return minEntry[1].toString();
     }
 
 }
--- day7/part2.ts
+++ day7/part2.ts
@@ -7,7 +7,27 @@ export default class Part2 extends AoCPart {
     async getAnswer(): Promise<string> {
         const lines = await this.getLines();
 
-        return "";
+        const nums: number[] = lines[0].split(",").map(num => +num);
+
+        const obj = {};
+        const max = nums.reduce((a, b) => Math.max(a, b));
+        for (let i = 0; i <= max; i++) {
+            let fuel = 0;
+            for (let j = 0; j < nums.length; j++) {
+                const difference = Math.abs(i - nums[j]);
+                fuel += (difference * (difference + 1)) / 2
+            }
+            obj[i] = fuel;
+        }
+
+        const entries = Object.entries(obj);
+        let minEntry = entries[0];
+        for (const entry of entries) {
+            if (entry[1] < minEntry[1])
+                minEntry = entry;
+        }
+
+        return minEntry[1].toString();
     }
 
 }
